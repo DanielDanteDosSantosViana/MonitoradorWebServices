41cb84e7fd4507dc422bcc2889ef390cb2ac6c76
DanielDanteDosSantosViana <danielvianarj@gmail.com>
2016-04-23 00:57:22 -0300

    Line command color

--- agent/lib/init/init.js
+++ agent/lib/init/init.js
@@ -16,16 +16,15 @@ function init(email,senha){
   utils.log("info","Autenticando usuário...");
 
   utils.config.existsFile(function(exists){
-
       if(!exists){
         protocol.authentication(new configUser(email,senha,null),function(response){
             utils.log("status","Usuário autenticado[OK]");
             utils.log("info","Gerando arquivo de configuração...");
             var token = response.body.Token;
 
-            utils.log("info",token);
             utils.config.writeFileConfig(JSON.stringify(new configUser(email,senha,token)),function(err,msg){
                     utils.log("status","Arquivo Gerado[OK]");
+                    utils.log("status","Token de acesso aos dados: "+token);
             });
           }, function(error,msg){
               utils.log("error",msg);
@@ -35,52 +34,6 @@ function init(email,senha){
       }
   });
 
-
-
-
-
- // utils.log("status","Configurações feitas com sucesso[OK]");
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  utils.log("info","gerando arquivo de configurações...");
-  utils.config.writeFileConfig(JSON.stringify(new data(email,senha)),
-    function(error,msg){
-    utils.log("info",error);
-
-    if(error!=null){
-      utils.log(error,msg);
-    }else{
-        utils.log("info","arquivo de configurações gerado.");
-        utils.log("info","autenticando usuário...");
-        protocol.authentication(function(response){
-            if(response.body.token!=null){
-              utils.log("info","capturando pacotes HTTP ...");
-              monitor.captor(function(lines){
-                   protocol.senderHTTP("post",JSON.stringify(lines),function(response){
-                    console.log(response.body);
-                 });
-              });
-            }else{
-              utils.log("error","Usuário não autenticado");
-            }
-          },function(error,msg){
-            console.log(msg)
-        });
-   }
-  });
-  */
 };
 
 
